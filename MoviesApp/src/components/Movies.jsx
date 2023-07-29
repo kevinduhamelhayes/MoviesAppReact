62e77863e651bbd2f845f24d76a0794728a5e276
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-07-29 13:02:21 -0300

    second commit

---
+++ MoviesApp/src/components/Movies.jsx
@@ -0,0 +1,21 @@
+function ListOfMovies({ movies }) {
+  return (
+    <ul className="movies">
+      {movies.map( movie => (
+        <li key={movie.id} className="movie">
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+          <img src={movie.poster} alt={movie.title} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+function NoMoviesResults() {
+  return <p> no hay peliculas</p>
+}
+export function Movies({ movies }) {
+  const hasMovies = movies?.length > 0
+
+  return hasMovies ? <ListOfMovies movies={movies} /> : <NoMoviesResults />
+}
