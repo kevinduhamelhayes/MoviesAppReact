8ebbc0b1f5e6a030d5c7fd5351b22e7305c58338
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-07-30 18:53:05 -0300

    act

--- MoviesApp/src/App.jsx
+++ MoviesApp/src/App.jsx
@@ -1,17 +1,35 @@
 import "./app.css"
-import { useRef } from "react"
+import { useEffect, useState } from "react"
 import { Movies } from "./components/Movies"
 import { useMovies } from "./hooks/useMovies"
+
 function App() {
-  const { movies: mappedMovies } = useMovies()
-  const inputRef = useRef()
+  const { movies } = useMovies()
+  const [query, setQuery] = useState("")
+  const [error, setError] = useState("")
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const fields = new FormData(event.target)
-    const query = fields.get("query")
-    console.log(query)
+    console.log({ query })
   }
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+  }
+  useEffect(() => {
+    if (query.length === 0) {
+      setError("No se ha ingresado un valor")
+      return
+    }
+    if (query.length < 3) {
+      setError("Ingrese mas de 3 caracteres")
+      return
+    }
+    if (query.match(/^[0-9]+$/)) {
+      setError("No se permiten numeros")
+      return
+    }
+    setError(null)
+  }, [query])
 
   return (
     <div className="page">
@@ -19,17 +37,19 @@ function App() {
         <h1>Buscador de Peliculas</h1>
         <form className="form" onSubmit={handleSubmit}>
           <input
+            onChange={handleChange}
+            value={query}
             name="query"
-            ref={inputRef}
             type="text"
             placeholder="avengers, stars wars, ........"
           />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color:"red" }}>{error}</p>}
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   )
